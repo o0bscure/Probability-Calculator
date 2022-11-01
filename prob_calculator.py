7bc104ff4f4e7fccb1cd4b9ca6feb03031fa7af2
M.Ghanem <86367386+o0bscure@users.noreply.github.com>
2022-11-01 20:51:49 +0300

    Update prob_calculator.py

--- prob_calculator.py
+++ prob_calculator.py
@@ -5,13 +5,24 @@ class Hat:
     
     #with each object created(hat), create a dictionary that has the ball color as key, and the number of balls with that particular color as value
     def __init__(self,**kwargs):
-        self.contents = dict()
+        #contents is list of items represent each ball in the hat
+        self.contents = []
+        #dictionary will represent the hats content in key, value pairs
+        self.dictionary = dict()
+        #for each color and its amount of balls
         for k,v in kwargs.items():
-            self.contents[k] = v
+            #adds items to the dictionary (to be used later in the repr function)
+            self.dictionary[k] = v
+            for i in range(v):
+                #adds each ball indiviually to the content list (by appending the key(color) for value(amount))
+                self.contents.append(k)
             
     #represent the object's dictionary when printing the object         
     def __repr__(self):
-        return f"{self.contents}"
+        output= ""
+        for color,amount in self.dictionary.items():
+            output = output + f"{color} : {amount}\n"
+        return output
         
     
     
@@ -23,6 +34,7 @@ hat1 = Hat(yellow=3, blue=2, green=6)
 hat2 = Hat(red=5, orange=4)
 hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
 
+print(hat1.contents)
 print(hat1)
-print(hat2)
-print(hat3)
+#print(hat2)
+#print(hat3)
