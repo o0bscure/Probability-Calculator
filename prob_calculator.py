bcebb55cfd5bdbf39c62bed408a57dbe83a15edb
M.Ghanem <86367386+o0bscure@users.noreply.github.com>
2022-11-06 21:42:16 +0300

    Update prob_calculator.py

--- prob_calculator.py
+++ prob_calculator.py
@@ -36,6 +36,8 @@ class Hat:
                 self.result.append(self.temp_lst[random_index])
                 #remove that item from the hat
                 self.temp_lst.pop(random_index)
+            if self.balls > len(self.temp_lst):
+                self.temp_lst = self.contents
                 
     #represent the object's dictionary when printing the object         
     def __repr__(self):
@@ -49,23 +51,29 @@ class Hat:
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     #need to check if the desired balls exist in the results after each draw(), keep track of how many times you got the desired result(M?) 
+    m = 0
+    probability = 0
     for experiment in range(num_experiments):
         #probability = (hat.dictionary["red"]+hat.dictionary["green"]) / len(hat.contents)
         hat.draw(num_balls_drawn)
-        if len(hat.contents) < 1: break
-        print(probability)
-    
+        for key,value in expected_balls.items():
+            
+        #keeps track if the desired result accurred
+        if hat.result.count("red") >= 2 and hat.result.count("green") >=1:
+            m = m + 1
+        if len(hat.temp_lst) < 1:
+            hat.temp_lst = hat.contents
+    probability = m/num_experiments
+    return probability
     
 hat1 = Hat(black=6, red=4, green=3)
 hat2 = Hat(red=5, orange=4)
 hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
 
-#probability = experiment(hat=hat1,
-                  #expected_balls={"red":2,"green":1},
-                  #num_balls_drawn=5,
-                  #num_experiments=10)
-hat1.draw(2)
-print(hat1.temp_lst)
-print(hat1.result)
+probability = experiment(hat=hat1,
+                  expected_balls={"red":2,"green":1},
+                  num_balls_drawn=5,
+                  num_experiments=2000)
+print(probability)
 
 
